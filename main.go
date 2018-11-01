5731a184be0f1479c74bd1d57a026d858f7e7b73
shortaflip <kevin.enario.354@my.csun.edu>
2018-11-01 14:07:20 -0700

    added the files for the controllers.

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"github.com/videoGameLibrary/videogamelibrary/config/database"
+)
+
 func main() {
 	// database.MigrateDependencyTables()
 	// database.MigrateTables()
+
+	defer database.GormConn.Close()
 }
