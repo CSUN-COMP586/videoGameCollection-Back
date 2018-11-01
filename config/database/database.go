d55358a8c2577104b7aa4e05a7086e770beeba46
shortaflip <kevin.enario.354@my.csun.edu>
2018-11-01 14:06:04 -0700

    deleted migrate.go and moved functions to database.go, this is now the only dependency for database connection.

--- config/database/database.go
+++ config/database/database.go
@@ -1,28 +1,51 @@
 package database
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/videoGameLibrary/videogamelibrary/businesslogic"
 )
 
 // local testing
 const dbConnectionString = "user=vglibdev password=abc123vglib dbname=vglib sslmode=disable"
 
-// GormConn connection available to all packages
 var GormConn *gorm.DB
-var dialect = "postgres"
 
-func openDatabaseConn() {
+func OpenDatabaseConn() {
 	var err error
+	var dialect = "postgres"
 
 	GormConn, err = gorm.Open(dialect, dbConnectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Connection established")
+}
+
+// MigrateDependencyTables - These functions should be used in their respective order
+// and should only be ran to create or recreate the tables in the database.
+func MigrateDependencyTables() {
+	GormConn.DropTableIfExists(
+		&businesslogic.Account{}, &businesslogic.Developer{},
+		&businesslogic.People{}, &businesslogic.Publisher{})
+	GormConn.AutoMigrate(
+		&businesslogic.Account{}, &businesslogic.Developer{},
+		&businesslogic.People{}, &businesslogic.Publisher{})
+}
+
+// MigrateTables - as above
+func MigrateTables() {
+	GormConn.DropTableIfExists(
+		&businesslogic.Game{}, &businesslogic.Character{},
+		&businesslogic.History{}, &businesslogic.Search{})
+	GormConn.AutoMigrate(
+		&businesslogic.Game{}, &businesslogic.Character{},
+		&businesslogic.History{}, &businesslogic.Search{})
 }
 
 func init() {
-	openDatabaseConn()
+	OpenDatabaseConn()
 }
