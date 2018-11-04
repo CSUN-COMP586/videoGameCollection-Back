c80c69a07468a8e09dac036b2295595f604c7a77
shortaflip <kevin.enario.354@my.csun.edu>
2018-11-04 10:42:21 -0800

    Created environment variables for database connection.

--- config/database/database.go
+++ config/database/database.go
@@ -1,51 +1,68 @@
 package database
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/videoGameLibrary/videogamelibrary/businesslogic"
 )
 
-// local testing
-const dbConnectionString = "user=vglibdev password=abc123vglib dbname=vglib sslmode=disable"
+// initialize environment variables
+var (
+	DBNAME             = getEnv("DBNAME", "vglib")
+	DBUSER             = getEnv("DBUSER", "vglibdev")
+	DBPASS             = getEnv("DBPASS", "abc123vglib")
+	dbConnectionString = "user=" + DBUSER + " password=" + DBPASS + " dbname=" + DBNAME + " sslmode=disable"
+	dialect            = "postgres"
+)
 
+// this pointer variable is initialized with the database connection and can then be called
+// for a dependency injection in other packages
 var GormConn *gorm.DB
 
-func OpenDatabaseConn() {
-	var err error
-	var dialect = "postgres"
+func init() {
+	GormConn = openDatabaseConnection(dialect, dbConnectionString)
+}
 
-	GormConn, err = gorm.Open(dialect, dbConnectionString)
+// private functions for connecting to the database
+func openDatabaseConnection(dialect string, dbConnectionString string) *gorm.DB {
+	var db *gorm.DB
+	var err error
+
+	db, err = gorm.Open(dialect, dbConnectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Connection established")
+
+	return db
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
 }
 
 // MigrateDependencyTables - These functions should be used in their respective order
 // and should only be ran to create or recreate the tables in the database.
-func MigrateDependencyTables() {
-	GormConn.DropTableIfExists(
-		&businesslogic.Account{}, &businesslogic.Developer{},
-		&businesslogic.People{}, &businesslogic.Publisher{})
-	GormConn.AutoMigrate(
-		&businesslogic.Account{}, &businesslogic.Developer{},
-		&businesslogic.People{}, &businesslogic.Publisher{})
-}
+// func MigrateDependencyTables() {
+// 	GormConn.DropTableIfExists(
+// 		&businesslogic.Account{}, &businesslogic.Developer{},
+// 		&businesslogic.People{}, &businesslogic.Publisher{})
+// 	GormConn.AutoMigrate(
+// 		&businesslogic.Account{}, &businesslogic.Developer{},
+// 		&businesslogic.People{}, &businesslogic.Publisher{})
+// }
 
-// MigrateTables - as above
-func MigrateTables() {
-	GormConn.DropTableIfExists(
-		&businesslogic.Game{}, &businesslogic.Character{},
-		&businesslogic.History{}, &businesslogic.Search{})
-	GormConn.AutoMigrate(
-		&businesslogic.Game{}, &businesslogic.Character{},
-		&businesslogic.History{}, &businesslogic.Search{})
-}
-
-func init() {
-	OpenDatabaseConn()
-}
+// // MigrateTables - as above
+// func MigrateTables() {
+// 	GormConn.DropTableIfExists(
+// 		&businesslogic.Game{}, &businesslogic.Character{},
+// 		&businesslogic.History{}, &businesslogic.Search{})
+// 	GormConn.AutoMigrate(
+// 		&businesslogic.Game{}, &businesslogic.Character{},
+// 		&businesslogic.History{}, &businesslogic.Search{})
+// }
---
+++ config/database/database.sh
@@ -0,0 +1,5 @@
+# source ./database.sh to run script and save new environment variables for database
+
+export DBNAME="vglib";
+export DBUSER="vglibdev";
+export DBPASSWORD="abc123vglib";
